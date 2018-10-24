b32750af1e6fb776868c2c76797adb6a673de74f
Jakob Voss <voss@gbv.de>
2018-10-24 13:44:09 +0200

    Extend access page (#3)

--- access.md
+++ access.md
@@ -3,21 +3,30 @@ title: Access
 layout: page
 ---
 
-Bibliographic data from Wikidata can be [accessed in multiple ways](https://www.wikidata.org/wiki/Wikidata:Data_access) such as [SPARQL](https://query.wikidata.org), and [database dumps](https://www.wikidata.org/wiki/Wikidata:Database_download). Several clients exist to simplify data access. [Scholia](https://tools.wmflabs.org/scholia/) gives a good overview of possibilities.
+Bibliographic data from Wikidata can be [accessed in multiple ways](https://www.wikidata.org/wiki/Wikidata:Data_access) such as [SPARQL](https://query.wikidata.org), and [database dumps](https://www.wikidata.org/wiki/Wikidata:Database_download). Several clients exist to simplify data access and to use the data. [Scholia](https://tools.wmflabs.org/scholia/) gives a good overview of possibilities.
 
 The following form uses [citation.js](https://citation.js.org/) to read and process bibliographic items from Wikidata.
 
-<div style="border: 1px solid #ddd; padding: 0em 0.5em">
+
+<p>
+ <div>
+  Examples: <ul id="examples" class="inline"></ul>
+ </div>
+</p>
+
 <p>
  <form onSubmit="return lookupWikidata(this.id.value)">
   <input type="text" name="id" placeholder="Wikidata item, e.g. Q3290152">
   <select id="formats"></select>
   <button type="submit">Submit</button>
   <span><i id="message"></i></span>
-  <a href="" id="link"></a>
  </form>
 </p>
-<p id="reference"></p>
-</div>
+<p id="cited-item" style="display:none">
+  <a href="" id="wikidata-link" class="wikidata-link">Wikidata item</a>,
+  <a href="" id="scholia-link">Scholia profile</a>,
+  <a href="" id="wikidata-json-link">Wikidata JSON</a>
+</p>
+<p id="citation" style="display:none"></p>
 
 <script src="../assets/access.js"></script>
--- assets/access.js
+++ assets/access.js
@@ -1,29 +1,56 @@
 const Cite = require('citation-js')
+
+let citeFormat = 'bibliography'
+let citeFormatOptions = {
+  format: 'html',
+  template: 'apa',
+  lang: 'en-US'
+}
+
+const citeExamples = [
+  'Q163335',
+  'Q3020388',
+  'Q3290152'
+]
+
 var item = null
 const formats = {
   apa: "American Psychological Association (APA)",
-  //bibtex: "BibTeX",
+  harvard1: "Harvard"
+}
+
+function notFound() {
+  $('#message').text("item not found")
 }
 
 function lookupWikidata(id) {
   let match = id.match(/(Q[1-9][0-9]+)/) 
   id = match ? match[1] : null
 
-  $('#link').hide()
-  $('#reference').hide()
+  $('form > input:first').val(id)
+
+  $('#cited-item').hide()
+  $('#citation').hide()
   $('#message').text('loading, please wait...')
 
-  item = id ? new Cite(id) : null
-  if (item) {
+  Cite.async(id)
+  .then(function(data) {
     $('#message').text('')
-    $('#link').attr('href', 'http://www.wikidata.org/entity/'+id)
-    $('#link').text(id)
-    $('#link').show()
+
+    // FIXME: citation.js always returns an instance?
+    if (!data || data.getIds()[0] !== id) {
+      notFound()
+      return
+    } else {
+      item = data
+    }
+
+    $('#wikidata-link').attr('href', 'http://www.wikidata.org/entity/'+id)
+    $('#scholia-link').attr('href', 'https://tools.wmflabs.org/scholia/work/'+id)
+    $('#wikidata-json-link').attr('href', 'http://www.wikidata.org/entity/'+id+'.json')
+    $('#cited-item').show()
     showItem()
-  } else {
-    $('#message').text("item not found")
-    return false
-  }
+  }).catch(notFound)
 
   return false
 }
@@ -32,25 +59,40 @@ function showItem() {
   if (!item) return
 
   let format = $('#formats').val()
-  if (!formats[format]) {
-    $('#formats').val('apa')
-  }
 
-  // FIXME: copy from https://larsgw.github.io/citation.js-form/demo/
-  let output = item.format('bibliography', {
-    format: 'html',
-    template: format,
-    lang: 'en-US'
-  })
+  if (format === 'data' || format === 'bibtex') {
+    citeFormat = format
+  } else {
+    if (!formats[format]) {
+      format = 'apa'
+      $('#formats').val('apa')
+    }
+    citeFormat = 'bibliography'
+    citeFormatOptions.format = 'html'
+    citeFormatOptions.template = format
+  }
   
-  $('#reference').html(output).show()
+  $('#citation').show().html(item.format(citeFormat, citeFormatOptions))
 }
 
 $(function () {
+  let examplesList = $("#examples")
+  citeExamples.forEach( function(id) {
+    examplesList.append(
+      $('<li>').append(
+        $('<a>').text(id).click(function() {
+          lookupWikidata(id)
+        })
+      )
+    )
+  })
+
   let select = $("#formats")
   $.each(formats, function(value, text) {
-    select.append(new Option(text, value));
+    select.append(new Option(text, value))
   })
+  select.append(new Option('BibTeX', 'bibtex'))
+  select.append(new Option('CLS-JSON', 'data'))
   select.change(showItem)
 
   const urlParams = new URLSearchParams(window.location.search)
@@ -60,7 +102,6 @@ $(function () {
   }
   let id = urlParams.get('id')
   if (id) {
-    $('form > input:first').val(id)
     lookupWikidata(id)
   }
 })
